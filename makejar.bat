6b37d5aa5a6a16a6e6c488368ed134f5b98a8390
vdisk <vdisk@foxmail.com>
2023-12-25 21:52:24 +0800

    feat: maven

--- makejar.bat
+++
@@ -1,4 +0,0 @@
-@cd build
-jar cvfm HentaiAtHome.jar ../src/hath/base/HentaiAtHome.manifest hath/base
-jar cvfm HentaiAtHomeGUI.jar ../src/hath/gui/HentaiAtHomeGUI.manifest ../src/hath/gui/*.png hath/gui
-@cd ..
